40d2d9cd0a99a98cbc85a88761d1442e0ae9ff58
ChronosCultus <Arann@kth.se>
2023-02-26 04:46:54 +0100

    Inkluderade en popup fönster med alternativ som frågar vilken svårighetsgrad man vill köra suduko på När man trycker på alternativet så tillkallar den switch och enum värden på SudokuUtilities.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.labb_4iamtostupidtocode {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.labb_4iamtostupidtocode to javafx.fxml;
